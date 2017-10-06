mildred/cjdnserver

commit 5fa6b46df4ed6d15406df74144c90e054dafee29

Add ability for the client to send its private key to the server

cmd/cjdnserver/cjdroute.go
@@ -3,12 +3,13 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"github.com/fc00/go-cjdns/key"
 	"log"
 	"os"
 	"os/exec"
 )
 
-func Genconf(cjdroute, tunsockpath, adminaddr string, peer *Peer) (string, string, error) {
+func Genconf(cjdroute, tunsockpath, adminaddr string, peer *Peer, skey *key.Private) (string, string, error) {
 	cmd := exec.Command("sh", "-xc", cjdroute+" --genconf | "+cjdroute+" --cleanconf")
 
 	out, err := cmd.Output()
@@ -21,6 +22,12 @@ func Genconf(cjdroute, tunsockpath, adminaddr string, peer *Peer) (string, strin
 	err = json.Unmarshal(out, &config)
 	if err != nil {
 		return "", "", err
+	}
+
+	if skey != nil {
+		config["privateKey"] = skey.String()
+		config["publicKey"] = skey.Pubkey().String()
+		config["ipv6"] = skey.Pubkey().IP().String()
 	}
 
 	interfaces_udp_0 := config["interfaces"].(map[string]interface{})["UDPInterface"].([]interface{})[0].(map[string]interface{})

cmd/cjdnserver/server.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"github.com/fc00/go-cjdns/key"
 	"github.com/jbenet/go-reuseport"
 	"github.com/mildred/cjdnserver"
 	"github.com/mildred/simpleipc"
@@ -105,13 +106,6 @@ func run(ctx context.Context, wg *sync.WaitGroup, cjdroute, sockPath string, per
 func handleClient(ctx0 context.Context, wg *sync.WaitGroup, cnx *net.UnixConn, cjdroute string, peer *Peer) error {
 	ctx, cancel := context.WithCancel(ctx0)
 
-	tmpdir, err := ioutil.TempDir("", "cjdnserver-client")
-	if err != nil {
-		return err
-	}
-	log.Printf("Receive client in %v", tmpdir)
-	defer os.RemoveAll(tmpdir)
-
 	adminif, err := reuseport.ListenPacket("udp", "127.0.0.1:0")
 	if err != nil {
 		return err
@@ -120,8 +114,38 @@ func handleClient(ctx0 context.Context, wg *sync.WaitGroup, cnx *net.UnixConn, c
 	log.Printf("Listen to admin %v", adminaddr)
 	defer adminif.Close()
 
+	h := new(simpleipc.Header)
+	privkey, err := h.ReadWithPayload(cnx, nil)
+	if err != nil {
+		return err
+	}
+	var skey *key.Private
+	if len(privkey) > 0 {
+		skey = new(key.Private)
+		if len(privkey) == len(*skey) {
+			copy(skey[:], privkey)
+		} else {
+			skey = nil
+		}
+	}
+	log.Printf("Received header %#v", h)
+	if len(h.Files) == 0 {
+		return fmt.Errorf("Did not received any file descriptor")
+	}
+
+	suffix := ""
+	if skey != nil {
+		suffix = "-" + skey.Pubkey().IP().String()
+	}
+	tmpdir, err := ioutil.TempDir("", "cjdnserver-client"+suffix)
+	if err != nil {
+		return err
+	}
+	log.Printf("Receive client in %v", tmpdir)
+	defer os.RemoveAll(tmpdir)
+
 	sockpath := path.Join(tmpdir, "cjdnstun.socket")
-	conf, ipv6, err := Genconf(cjdroute, sockpath, adminaddr, peer)
+	conf, ipv6, err := Genconf(cjdroute, sockpath, adminaddr, peer, skey)
 	if err != nil {
 		return err
 	}
@@ -135,20 +159,11 @@ func handleClient(ctx0 context.Context, wg *sync.WaitGroup, cnx *net.UnixConn, c
 	log.Printf("Configuration file written to %s", conffile)
 	log.Print(conf)
 
-	h := new(simpleipc.Header)
-	err = h.Read(cnx, nil)
-	if err != nil {
-		return err
-	}
-	log.Printf("Received header %#v", h)
-	if len(h.Files) == 0 {
-		return fmt.Errorf("Did not received any file descriptor")
-	}
 	tunfd, err := MakeTunInNs(h.Files[0], ipv6, InterfaceMTU)
-	_ = tunfd
 	if err != nil {
 		return err
 	}
+	defer tunfd.Close()
 
 	go (func() {
 		err := SendTunDev(sockpath, tunfd)
@@ -193,13 +208,14 @@ func handleClient(ctx0 context.Context, wg *sync.WaitGroup, cnx *net.UnixConn, c
 		case <-ctx.Done():
 			log.Printf("Send SIGTERM to cjdroute")
 			process.Signal(syscall.SIGTERM)
-			break
 		case err := <-cerr:
 			log.Printf("Error: %s", err)
 		case state := <-cstate:
 			log.Printf("Terminated: %s", state.String())
 		}
 	}
+
+	log.Printf("Stopped %s", ipv6)
 
 	return nil
 }
@@ -212,7 +228,7 @@ func receiveWatchdog(ctx0 context.Context, wg *sync.WaitGroup, cancel context.Ca
 		//defer wg.Done()
 		for ctx.Err() == nil {
 			h := new(simpleipc.Header)
-			err := h.Read(cnx, nil)
+			_, err := h.ReadWithPayload(cnx, nil)
 			if err != nil {
 				log.Printf("Received error: %s", err)
 				log.Printf("Watchdog triggered stop")
@@ -248,6 +264,7 @@ func SendTunDev(sockPath string, tunfd *os.File) error {
 			attempts++
 			continue
 		}
+		defer cnx0.Close()
 		cnx := cnx0.(*net.UnixConn)
 
 		h := simpleipc.NewHeader(0, 0, []*os.File{tunfd})

cmd/cjdnsclient/client.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
+	"github.com/fc00/go-cjdns/key"
 	"github.com/mildred/cjdnserver"
 	"github.com/mildred/simpleipc"
 	"log"
@@ -17,8 +19,10 @@ import (
 func main() {
 	var sockPath string
 	var watchdog bool
+	var privkey string
 	flag.StringVar(&sockPath, "sock", "cjdserver.sock", "Socker file path")
 	flag.BoolVar(&watchdog, "watchdog", false, "internal use")
+	flag.StringVar(&privkey, "privkey", "", "private key")
 	flag.Parse()
 
 	var wg sync.WaitGroup
@@ -36,14 +40,26 @@ func main() {
 			log.Fatal(err)
 		}
 	} else {
-		err := run(ctx, &wg, sockPath)
+		err := run(ctx, &wg, sockPath, privkey)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
 }
 
-func run(ctx context.Context, wg *sync.WaitGroup, sockPath string) error {
+func run(ctx context.Context, wg *sync.WaitGroup, sockPath, privkey string) error {
+	var err error
+	var skey *key.Private = nil
+
+	if privkey != "" {
+		skey, err = key.DecodePrivate(privkey)
+		if err != nil {
+			return err
+		} else if !skey.Valid() {
+			return fmt.Errorf("invalid private key")
+		}
+	}
+
 	cnx0, err := net.Dial("unix", sockPath)
 	if err != nil {
 		return err
@@ -58,7 +74,14 @@ func run(ctx context.Context, wg *sync.WaitGroup, sockPath string) error {
 
 	log.Printf("Connected to server %v", cnx)
 	h := simpleipc.NewHeader(cjdnserver.InitialRequest, 0, []*os.File{netns})
-	err = h.Write(cnx)
+	if skey != nil {
+		h.Size = uint32(len(*skey))
+	}
+	if skey != nil {
+		err = h.WriteWithPayload(cnx, skey[:])
+	} else {
+		err = h.WriteWithPayload(cnx, nil)
+	}
 	if err != nil {
 		return err
 	}

commit 0253fd8d84873cac3bf9f9cd441835afed162073

Fix bug introduced in 5fa6b46df4ed6d15406df74144c90e054dafee29

cmd/cjdnserver/cjdroute.go
@@ -38,8 +38,8 @@ func Genconf(cjdroute, tunsockpath, adminaddr string, peer *Peer, skey *key.Priv
 			"publicKey": peer.Pubkey,
 		}
 	}
-	logging := config["logging"].(map[string]interface{})
-	logging["logTo"] = "stdout"
+	//logging := config["logging"].(map[string]interface{})
+	//logging["logTo"] = "stdout"
 	admin := config["admin"].(map[string]interface{})
 	admin["bind"] = adminaddr
 	adminpass := admin["password"].(string)

cmd/cjdnserver/server.go
@@ -186,6 +186,7 @@ func handleClient(ctx0 context.Context, wg *sync.WaitGroup, cnx *net.UnixConn, c
 	}()
 
 	for ctx.Err() == nil {
+		instanceCtx, instanceStop := context.WithCancel(ctx)
 		log.Printf("Start cjdroute")
 		process, err := Start(cjdroute, conf)
 		if err != nil {
@@ -193,10 +194,12 @@ func handleClient(ctx0 context.Context, wg *sync.WaitGroup, cnx *net.UnixConn, c
 		}
 
 		go (func() {
-			err := SendTunDev(sockpath, tunfd)
+			cnx, err := SendTunDev(sockpath, tunfd)
 			if err != nil {
 				log.Print(err)
 			}
+			defer cnx.Close()
+			<-instanceCtx.Done()
 		})()
 
 		h = simpleipc.NewHeader(cjdnserver.InitialResponse, 0, nil)
@@ -236,6 +239,7 @@ func handleClient(ctx0 context.Context, wg *sync.WaitGroup, cnx *net.UnixConn, c
 		case state := <-cstate:
 			log.Printf("Terminated: %s", state.String())
 		}
+		instanceStop()
 	}
 
 	log.Printf("Stopped %s", ipv6)
@@ -276,7 +280,7 @@ func receiveWatchdog(ctx0 context.Context, wg *sync.WaitGroup, cancel context.Ca
 	}
 }
 
-func SendTunDev(sockPath string, tunfd *os.File) error {
+func SendTunDev(sockPath string, tunfd *os.File) (net.Conn, error) {
 	attempts := 0
 	var err error
 	for attempts < 1000 {
@@ -287,15 +291,14 @@ func SendTunDev(sockPath string, tunfd *os.File) error {
 			attempts++
 			continue
 		}
-		defer cnx0.Close()
 		cnx := cnx0.(*net.UnixConn)
 
 		h := simpleipc.NewHeader(0, 0, []*os.File{tunfd})
 		err = h.Write(cnx)
 		if err != nil {
-			return err
+			return nil, err
 		}
-		return nil
+		return cnx0, nil
 	}
-	return err
+	return nil, err
 }